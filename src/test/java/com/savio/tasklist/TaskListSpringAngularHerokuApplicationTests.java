655ce1237cee5712fd349d79f4fc5be46b76fa1b
goanpao <sav.extra@gmail.com>
2016-05-23 14:30:20 -0400

    Changed incorrect packages

--- src/test/java/com/savio/tasklist/TaskListSpringAngularHerokuApplicationTests.java
+++ src/test/java/com/savio/tasklist/TaskListSpringAngularHerokuApplicationTests.java
@@ -1,9 +1,11 @@
-package com.savio.tasklist;
+package test.java.com.savio.tasklist;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.savio.tasklist.TaskListSpringAngularHerokuApplication;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = TaskListSpringAngularHerokuApplication.class)
